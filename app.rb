9b47fd6adea2ea43b66bea8de78acbd2a7e652fc
craigwann <craigwann@me.com>
2018-07-06 09:02:12 -0700

    build basic views .erb's with form

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sinatra/reloader'
+also_reload 'lib/**/*.rb'
+require('./lib/words')
+require('pry')
+
+get('/') do
+  @list = Word.all()
+  erb(:input)
+end
+
+post('/') do
+  word = params["word"]
+  definition = params["definition"]
+  item = Word.new(word)
+  item.save()
+  @list = Word.all()
+  erb(:input)
+end
+
+get ('/items/:id') do
+  @item = Word.find(params[:id])
+  erb(:output)
+end
---
+++ lib/words.rb
@@ -0,0 +1,33 @@
+class Word
+  @@list = []
+  attr_reader :id
+  attr_accessor :word
+  attr_accessor :definition
+
+  def initialize(word)
+    @word = word
+    
+    @id = @@list.length + 1
+  end
+
+  def self.all()
+    @@list
+  end
+
+  def self.find(id)
+    item_id = id.to_i()
+    @@list.each do |item|
+      if item.id == item_id
+        return item
+      end
+    end
+  end
+
+  def save()
+      @@list.push(self)
+  end
+
+  def self.clear()
+    @@list = []
+  end
+end
