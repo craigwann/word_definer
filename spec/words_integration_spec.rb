83225b24de6ee86ba3c8be940a44031726e8fe11
craigwann <craigwann@me.com>
2018-07-08 13:27:36 -0700

    pass 5 rspec and 3 integration tests

--- spec/words_integration_spec.rb
+++ spec/words_integration_spec.rb
@@ -13,8 +13,8 @@ describe('1-int word/definition path', {:type => :feature}) do
     expect(page).to have_content('gravity')
   end
 end
-describe('2-int word/definition path', {:type => :feature}) do
-  it('processes the user entry and returns whether word is entered') do
+describe('2-int word/definition add definition path', {:type => :feature}) do
+  it('processes the user entry and returns whether additional definition is entered') do
     Word.clear()
     visit('/')
     fill_in('word', :with => 'dog')
@@ -24,5 +24,21 @@ describe('2-int word/definition path', {:type => :feature}) do
     fill_in('definition_add', :with => 'woof')
     click_button('Add definition')
     expect(page).to have_content('woof')
+  end
+end
+describe('3-int word/definition add definition path, return to input', {:type => :feature}) do
+  it('processes the user entry and returns whether word is entered') do
+    Word.clear()
+    visit('/')
+    fill_in('word', :with => 'dog')
+    fill_in('definition', :with => 'four legged mammal')
+    click_button('Add Word')
+    fill_in('word', :with => 'cat')
+    fill_in('definition', :with => 'four legged mammal')
+    click_link('dog')
+    fill_in('definition_add', :with => 'woof')
+    click_button('Add definition')
+    click_button('Word List')
+    expect(page).to have_content('dog')
   end
 end
